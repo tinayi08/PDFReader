e9023ac148a20904d9ffc20300923ac41b935979
tinayi08 <christinayi827@gmail.com>
2021-07-27 16:03:31 -0700

    methods updated to convert multiple files with proper file names in new documents

--- src/DocumentReader.java
+++ src/DocumentReader.java
@@ -14,15 +14,16 @@ public class DocumentReader {
     String returnString;
     File[] listOfFiles;
 
+
     //TODO - create an output file
     //TODO - create a loop so it goes file to file
     //TODO - Path name from another method too? pass through as a parameter?
     public DocumentReader() throws Exception {
-        //pdfReader("days");
+
         fileName();
         fileLoop();
-        createTextFile();
-        testCreatePDF();
+        System.out.println("Program end");
+
 
     }
 
@@ -31,28 +32,39 @@ public class DocumentReader {
         File pdfFile = new File("/Users/tinayi/Documents/Java/TestFiles/" + fileName);
 
 
-        PDDocument pdDoc = PDDocument.load(pdfFile);
-        String stripper = new PDFTextStripper().getText(pdDoc);
-        int end = stripper.indexOf(endText);
-        int length = endText.length();
-        pdDoc.getClass();
-        if (!pdDoc.isEncrypted()) {
-            PDFTextStripperByArea tStripper = new PDFTextStripperByArea();
-            tStripper.setSortByPosition(true);
-            returnString = stripper.substring(0, end+length);
-            //System.out.println(returnString);
+        PDDocument pdDoc = null;
+
+        try {
+            pdDoc = PDDocument.load(pdfFile);
+            String stripper = new PDFTextStripper().getText(pdDoc);
+            int end = stripper.indexOf(endText);
+            int length = endText.length();
+            pdDoc.getClass();
+            if (!pdDoc.isEncrypted()) {
+                PDFTextStripperByArea tStripper = new PDFTextStripperByArea();
+                tStripper.setSortByPosition(true);
+                returnString = stripper.substring(0, end+length);
+                //System.out.println(returnString);
+            }
+        } finally {
+            if (pdDoc != null) {
+                pdDoc.close();
+            }
+
         }
+
 
         return returnString;
 
     }
 
-    public void createTextFile() throws IOException {
+
+    public void createTextFile(String fileName) throws IOException {
         //TODO - Need to create a loop or something so i can have new file names
         FileWriter out = null;
 
         try {
-            out = new FileWriter("/Users/tinayi/Documents/Java/NewFile/test5.txt");
+            out = new FileWriter("/Users/tinayi/Documents/Java/NewFile/" + fileName + ".txt");
             out.write(returnString);
         } catch (IOException e) {
             e.printStackTrace();
@@ -65,13 +77,13 @@ public class DocumentReader {
 
     public void testCreatePDF() throws Exception {
 
-        String text = returnString.replace("\n", "n").replace("\r", "r");
-
+        String text = returnString.replace("\n", "n");
+        PDDocument doc = null;
         try {
             //TODO - Need to create a loop or something so i can have new file names
             //String fileName = "/Users/tinayi/Documents/Java/" + file; --- need to change file extension for output
             String fileName = "/Users/tinayi/Documents/Java/NewFile/test3.pdf";
-            PDDocument doc = new PDDocument();
+            doc = new PDDocument();
             PDPage page = new PDPage();
 
             doc.addPage(page);
@@ -91,6 +103,10 @@ public class DocumentReader {
             doc.close();
         } catch (IOException e) {
             e.printStackTrace();
+//        } finally {
+//            if (doc != null) {
+//                doc.close();
+//            }
         }
     }
 
@@ -105,15 +121,19 @@ public class DocumentReader {
     }
 
     public void fileLoop() throws Exception {
-
         for (File file : listOfFiles) {
-            if (file.isFile()) {
-
+            if (file.isFile() && file.getName() != ".DS_Store") {
                 pdfReader("days", file.getName());
+                createTextFile(removeExtensionFromFileName(file.getName()));
                 //System.out.println(file.getName());
             }
         }
 
+    }
+
+    public String removeExtensionFromFileName(String fileName) {
+        String newFileName = fileName.substring(0, fileName.length()-4);
+        return newFileName;
     }
 
 
